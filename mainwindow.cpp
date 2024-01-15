6a81a74ca0566d6eff2b1f765bc9a6c497315c2b
moughit <abdelmoughith2002@gmail.com>
2024-01-15 13:18:55 +0100

    project

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,22 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    setStyleSheet("QDialog { background-color: lightblue; }"
-                          "QPushButton { background-color: #4CAF50; color: white; }"
-                          "QComboBox { background-color: white; }"
-                          "QLabel { font-size: 12px; color: #333333; }");
+
+        // Set the stylesheet for the entire application
+    qApp->setStyleSheet(
+            "QMainWindow { background-color: #2C3E50; color: #ECF0F1; }"
+            "QDialog { background-color: #34495E; border: 2px solid #2C3E50; }"
+            "QPushButton { background-color: #3498DB; color: #ECF0F1; border: 1px solid #3498DB; padding: 8px; border-radius: 5px; }"
+            "QPushButton:hover { background-color: #2980B9; }"
+            "QComboBox { background-color: #2C3E50; color: #ECF0F1; border: 1px solid #2C3E50; padding: 5px; border-radius: 3px; }"
+            "QComboBox::drop-down { border: none; }"
+            "QComboBox::down-arrow { image: url(:/assets/img/down-arrow.png); }"
+            "QLabel { font-size: 12px; color: #ECF0F1; }"
+            "QLabel#lblInfoImage { border: 2px solid #3498DB; border-radius: 5px; }"
+        );
+
+
+
 
     QPixmap pm(":/assets/img/infoImage.jpg");
     ui->lblInfoImage->setPixmap(pm);
